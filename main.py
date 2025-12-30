278fdb06861d4917326a436742aeeed8ce796c2b
NandakrishnanR <rnandakrishnan2001@gmail.com>
2025-12-30 15:42:54 +0100

    ever

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 from flask import Flask,request,jsonify,url_for,render_template
 import pickle
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+redmodel=pickle.load(open('regmodel.pkl','rb'))'))
+scaler=pickle.load(open('scaler.pkl','rb'))
+@app.route('/')
+def home():
+    return render_template('home.html')
+@app.route('/predict',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    
+
+
