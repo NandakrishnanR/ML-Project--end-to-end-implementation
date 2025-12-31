0647f5127186e8a71180c19737b12c2ecf165d02
NandakrishnanR <rnandakrishnan2001@gmail.com>
2025-12-31 10:26:08 +0100

    working flask app

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 app=Flask(__name__)
-redmodel=pickle.load(open('regmodel.pkl','rb'))'))
+regmodel=pickle.load(open('regmodel.pkl','rb'))
 scaler=pickle.load(open('scaler.pkl','rb'))
 @app.route('/')
 def home():
@@ -12,6 +12,13 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict_api():
     data=request.json['data']
-    
+    print(data)
+    print(list(data.values())) #output 1‑D array: shape (n_features,)
+    print(np.array(list(data.values())).reshape(1,-1)) #because scikit‑learn models expect a 2‑D array shaped (n_samples, n_features)
+    new_Data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output= regmodel.predict(new_Data)
+    print(output[0])
+    return jsonify(output[0])
 
-
+if __name__ == "__main__":
+    app.run(debug=True)
